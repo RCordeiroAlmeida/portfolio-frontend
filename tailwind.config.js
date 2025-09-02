0cd3b266f796deb16f2318d80b6ada22ea8d797b
Rafael Cordeiro <rafaelcordeiro299@gmail.com>
2025-09-02 17:48:56 -0300

    Atualizando Tailwind, postcss e output.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
